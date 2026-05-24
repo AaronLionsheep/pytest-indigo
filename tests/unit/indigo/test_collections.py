b992ca6505828c8738c5c4d19c7f9ca3ced966c2
Aaron Rohrbaugh <aaron@lionsheeptechnology.com>
2026-05-23 22:52:32 -0400

    Fix ambiguous variable names

--- tests/unit/indigo/test_collections.py
+++ tests/unit/indigo/test_collections.py
@@ -9,35 +9,35 @@ COMPLEX_VALUES = [Exception("PYTEST"), object(), set([1, 2, 3])]
 
 
 def test_list_init_primitive_values(indigo: IndigoMock):
-    l = indigo.List(PRIMITIVE_VALUES)
+    ilist = indigo.List(PRIMITIVE_VALUES)
 
-    assert l == PRIMITIVE_VALUES
+    assert ilist == PRIMITIVE_VALUES
 
 def test_list_get_by_index(indigo: IndigoMock):
-    l = indigo.List("abcdefghijklmnopqrstuvwxyz")
+    ilist = indigo.List("abcdefghijklmnopqrstuvwxyz")
 
-    assert l[0] == "a"
-    assert l[25] == "z"
+    assert ilist[0] == "a"
+    assert ilist[25] == "z"
     
 @pytest.mark.parametrize("value", PRIMITIVE_VALUES)
 def test_list_appends_primitive_values(indigo: IndigoMock, value):
-    l = indigo.List(PRIMITIVE_VALUES)
-    l.append(value)
+    ilist = indigo.List(PRIMITIVE_VALUES)
+    ilist.append(value)
 
-    assert l == [*PRIMITIVE_VALUES, value]
+    assert ilist == [*PRIMITIVE_VALUES, value]
 
 def test_list_extends_primitive_values(indigo: IndigoMock):
-    l = indigo.List(PRIMITIVE_VALUES)
-    l.extend(PRIMITIVE_VALUES)
+    ilist = indigo.List(PRIMITIVE_VALUES)
+    ilist.extend(PRIMITIVE_VALUES)
 
-    assert l == [*PRIMITIVE_VALUES, *PRIMITIVE_VALUES]
+    assert ilist == [*PRIMITIVE_VALUES, *PRIMITIVE_VALUES]
 
 @pytest.mark.parametrize("value", PRIMITIVE_VALUES)
 def test_list_set_primitive_value(indigo: IndigoMock, value):
-    l = indigo.List(["PYTEST"])
-    l[0] = value
+    ilist = indigo.List(["PYTEST"])
+    ilist[0] = value
 
-    assert l == [value]
+    assert ilist == [value]
 
 def test_list_init_complex_values_raises_type_error(indigo: IndigoMock):
     with pytest.raises(TypeError):
@@ -45,27 +45,27 @@ def test_list_init_complex_values_raises_type_error(indigo: IndigoMock):
 
 @pytest.mark.parametrize("value", COMPLEX_VALUES)
 def test_list_append_complex_values_raises_type_error(indigo: IndigoMock, value):
-    l = indigo.List()
+    ilist = indigo.List()
     with pytest.raises(TypeError):
-        l.append(value)
+        ilist.append(value)
 
 def test_list_set_by_slice_raises_not_implemented_error(indigo: IndigoMock):
-    l = indigo.List()
+    ilist = indigo.List()
     with pytest.raises(NotImplementedError):
-        l[5:] = [1, 2, 3, 4, 5, 6] # type: ignore
+        ilist[5:] = [1, 2, 3, 4, 5, 6] # type: ignore
 
 def test_list_get_by_slice_raises_not_implemented_error(indigo: IndigoMock):
-    l = indigo.List()
+    ilist = indigo.List()
     with pytest.raises(NotImplementedError):
-        l[5:] # type: ignore
+        ilist[5:] # type: ignore
 
 def test_list_to_list_simple(indigo: IndigoMock):
-    l = indigo.List([1, 2, 3, 4, 5])
+    ilist = indigo.List([1, 2, 3, 4, 5])
 
-    assert l.to_list() == [1, 2, 3, 4, 5]
+    assert ilist.to_list() == [1, 2, 3, 4, 5]
 
 def test_list_to_list_converts_nested_lists_and_dicts(indigo: IndigoMock):
-    l = indigo.List(
+    ilist = indigo.List(
         [
             1,
             2,
@@ -78,7 +78,7 @@ def test_list_to_list_converts_nested_lists_and_dicts(indigo: IndigoMock):
         ]
     )
 
-    assert l.to_list() == [
+    assert ilist.to_list() == [
         1,
         2,
         [3, 4],
@@ -96,9 +96,9 @@ def test_list_values_return_copy(indigo: IndigoMock):
 
 @pytest.mark.parametrize("value", PRIMITIVE_VALUES)
 def test_dict_init_primitive_value(indigo: IndigoMock, value):
-    d = indigo.Dict({"value": value})
+    idict = indigo.Dict({"value": value})
 
-    assert d["value"] == value
+    assert idict["value"] == value
 
 @pytest.mark.parametrize("value", COMPLEX_VALUES)
 def test_dict_init_complex_value_raises_type_error(indigo: IndigoMock, value):
@@ -106,59 +106,59 @@ def test_dict_init_complex_value_raises_type_error(indigo: IndigoMock, value):
         indigo.Dict({"value": value})
 
 def test_dict_get_key(indigo: IndigoMock):
-    d = indigo.Dict()
-    d["key"] = 5
+    idict = indigo.Dict()
+    idict["key"] = 5
 
-    assert d.get("key") == 5
+    assert idict.get("key") == 5
 
 def test_dict_get_missing_key_returns_none_by_default(indigo: IndigoMock):
-    d = indigo.Dict()
+    idict = indigo.Dict()
 
-    assert d.get("key") is None
+    assert idict.get("key") is None
 
 def test_dict_get_missing_key_returns_default(indigo: IndigoMock):
-    d = indigo.Dict()
+    idict = indigo.Dict()
 
-    assert d.get("key", 5) == 5
+    assert idict.get("key", 5) == 5
 
 @pytest.mark.parametrize("key", ["abc123", "one23", "a/2", "A-_m!@"])
 def test_dict_keys(indigo: IndigoMock, key):
-    d = indigo.Dict()
-    d[key] = "PYTEST"
+    idict = indigo.Dict()
+    idict[key] = "PYTEST"
 
-    assert d[key] == "PYTEST"
+    assert idict[key] == "PYTEST"
 
 @pytest.mark.parametrize("key", [1, None, list()])
 def test_dict_non_string_key_raises_type_error(indigo: IndigoMock, key):
     with pytest.raises(TypeError):
-        d = indigo.Dict()
-        d[key] = "PYTEST"
+        idict = indigo.Dict()
+        idict[key] = "PYTEST"
 
 @pytest.mark.parametrize("key", [" ", "ab c"])
 def test_dict_key_with_space_raises_runtime_error(indigo: IndigoMock, key):
     with pytest.raises(RuntimeError, match="LowLevelBadParameterError -- illegal XML tag name character"):
-        d = indigo.Dict()
-        d[key] = "PYTEST"
+        idict = indigo.Dict()
+        idict[key] = "PYTEST"
 
 @pytest.mark.parametrize("key_start", list(string.digits + string.punctuation))
 def test_dict_key_starting_with_number_or_character_raises_runtime_error(indigo: IndigoMock, key_start):
     with pytest.raises(RuntimeError, match="LowLevelBadParameterError -- illegal XML tag name character"):
-        d = indigo.Dict()
-        d[key_start + "key"] = "PYTEST"
+        idict = indigo.Dict()
+        idict[key_start + "key"] = "PYTEST"
 
 @pytest.mark.xfail(reason="IndigoServer doesn't enforce this")
 @pytest.mark.parametrize("key", ["XML", "xml", "Xml"])
 def test_dict_xml_key_raises_runtime_error(indigo: IndigoMock, key):
     with pytest.raises(RuntimeError, match="LowLevelBadParameterError -- illegal XML tag name character"):
-        d = indigo.Dict()
-        d[key] = "PYTEST"
+        idict = indigo.Dict()
+        idict[key] = "PYTEST"
 
 @pytest.mark.parametrize("value", PRIMITIVE_VALUES)
 def test_dict_set_primitive_value(indigo: IndigoMock, value):
-    d = indigo.Dict()
-    d["test"] = value
+    idict = indigo.Dict()
+    idict["test"] = value
 
-    assert d["test"] == value
+    assert idict["test"] == value
 
 @pytest.mark.parametrize(
     "value",
@@ -169,10 +169,10 @@ def test_dict_set_primitive_value(indigo: IndigoMock, value):
     ]
 )
 def test_dict_set_container_values(indigo: IndigoMock, value):
-    d = indigo.Dict()
-    d["test"] = value
+    idict = indigo.Dict()
+    idict["test"] = value
 
-    assert d["test"] == value
+    assert idict["test"] == value
 
 @pytest.mark.parametrize(
     "value",
@@ -184,25 +184,25 @@ def test_dict_set_container_values(indigo: IndigoMock, value):
     ]
 )
 def test_dict_complex_container_values_raises_type_error(indigo: IndigoMock, value):
-    d = indigo.Dict()
+    idict = indigo.Dict()
     with pytest.raises(TypeError):
-        d["test"] = value
+        idict["test"] = value
 
 def test_dict_to_dict(indigo: IndigoMock):
-    d = indigo.Dict()
-    d["key"] = 5
-    result = d.to_dict()
+    idict = indigo.Dict()
+    idict["key"] = 5
+    result = idict.to_dict()
 
     assert result == {"key": 5}
 
 def test_dict_values_return_copy(indigo: IndigoMock):
-    d1 = indigo.Dict()
-    d1["a"] = 1
+    outer = indigo.Dict()
+    outer["a"] = 1
 
-    d2 = indigo.Dict()
-    d2["a"] = 1
-    d2["b"] = 2
+    inner = indigo.Dict()
+    inner["a"] = 1
+    inner["b"] = 2
 
-    d1["d2"] = d2
+    outer["d2"] = inner
 
-    assert d1["d2"] is not d2
+    assert outer["d2"] is not inner
