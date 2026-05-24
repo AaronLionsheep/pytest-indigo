2c7755d8461166bf0d420448a6a8cc0b06c0eed9
Aaron Rohrbaugh <aaron@lionsheeptechnology.com>
2026-05-23 20:39:26 -0400

    Complete dict and list

--- src/pytest_indigo/indigo/collections.py
+++ src/pytest_indigo/indigo/collections.py
@@ -1,11 +1,23 @@
 import string
-from typing import Any
+from typing import Any, SupportsIndex, overload, override, TypeAlias, Iterable, TypeVar, Generic
 from copy import deepcopy
 
 
+CollectionValue: TypeAlias = None | bool | float | int | str | list["CollectionValue"] | dict[str, "CollectionValue"]
+
+type_strings = {
+    None: "empty",
+    int: "integer",
+    float: "real",
+    str: "string",
+    bool: "bool",
+    list: "list",
+    dict: "dict"
+}
+
 def _validate_value(value: Any) -> None:
     """Validate collection values against documented rules"""
-    if isinstance(value, (bool, float, int, str)):
+    if value is None or isinstance(value, (bool, float, int, str)):
         return
 
     if isinstance(value, list):
@@ -20,15 +32,63 @@ def _validate_value(value: Any) -> None:
         )
 
 class List(list):
-    pass
+    """
+    Indigo list wrapper. Behaves like a regular Python list but may carry
+    additional Indigo metadata.
+    """
+    def __init__(self, iterable: Iterable[CollectionValue] | None = None, /) -> None:
+        if iterable is None:
+            super().__init__()
+        else:
+            _validate_value(iterable)
+            super().__init__(iterable)
+
+    def __setitem__(self, key: SupportsIndex, value: CollectionValue, /) -> None: # type: ignore
+        if isinstance(key, slice):
+            raise NotImplementedError()
+        
+        _validate_value(value)
+        super().__setitem__(key, value)
+
+    def __getitem__(self, key: SupportsIndex, /) -> CollectionValue: # type: ignore
+        if isinstance(key, slice):
+            raise NotImplementedError()
+
+        value = super().__getitem__(key)
+        return deepcopy(value)
+    
+    def append(self, object: CollectionValue, /) -> None:
+        _validate_value(object)
+        super().append(object)
+
+    def extend(self, iterable: Iterable[CollectionValue], /) -> None:
+        for item in iterable: _validate_value(item)
+        super().extend(iterable)
+
+    def to_list(self) -> list:
+        """Recursively convert to a plain Python list, converting any nested
+        indigo.Dict / indigo.List values and Indigo constants to their Python equivalents.
+        Patched in by utils.py.
+        """
+        return list(self)
 
 class Dict(dict):
+    """
+    Indigo dict wrapper. Behaves like a regular Python dict but may carry
+    additional Indigo metadata. Used for pluginProps, globalProps, sharedProps, etc.
+    """
+
     def get(self, key: str, default: Any = None) -> Any:
         return super().get(key, default)
     
     @staticmethod
     def __validate_key(key: str):
         """Validate keys against documented rules"""
+        if not isinstance(key, str):
+            raise TypeError(
+                f"No registered converter was able to produce a C++ rvalue of type CCString from this Python object of type {type(key)}"
+            )
+
         if key[0] in string.digits or key[0] in string.punctuation or " " in key:
             raise RuntimeError("LowLevelBadParameterError -- illegal XML tag name character")
     
@@ -44,4 +104,8 @@ class Dict(dict):
         return deepcopy(value)
 
     def to_dict(self) -> dict:
+        """Recursively convert to a plain Python dict, converting any nested
+        indigo.Dict / indigo.List values and Indigo constants to their Python equivalents.
+        Patched in by utils.py.
+        """
         return dict(self)
--- tests/unit/indigo/test_collections.py
+++ tests/unit/indigo/test_collections.py
@@ -3,6 +3,97 @@ import string
 
 from pytest_indigo import IndigoMock
 
+
+PRIMITIVE_VALUES = [None, True, False, 1.8, -7.5, 10, "", "pytest", list(), dict()]
+COMPLEX_VALUES = [Exception("PYTEST"), object(), set([1, 2, 3])]
+
+
+def test_list_init_primitive_values(indigo: IndigoMock):
+    l = indigo.List(PRIMITIVE_VALUES)
+
+    assert l == PRIMITIVE_VALUES
+
+def test_list_get_by_index(indigo: IndigoMock):
+    l = indigo.List("abcdefghijklmnopqrstuvwxyz")
+
+    assert l[0] == "a"
+    assert l[25] == "z"
+    
+@pytest.mark.parametrize("value", PRIMITIVE_VALUES)
+def test_list_appends_primitive_values(indigo: IndigoMock, value):
+    l = indigo.List(PRIMITIVE_VALUES)
+    l.append(value)
+
+    assert l == [*PRIMITIVE_VALUES, value]
+
+def test_list_extends_primitive_values(indigo: IndigoMock):
+    l = indigo.List(PRIMITIVE_VALUES)
+    l.extend(PRIMITIVE_VALUES)
+
+    assert l == [*PRIMITIVE_VALUES, *PRIMITIVE_VALUES]
+
+@pytest.mark.parametrize("value", PRIMITIVE_VALUES)
+def test_list_set_primitive_value(indigo: IndigoMock, value):
+    l = indigo.List(["PYTEST"])
+    l[0] = value
+
+    assert l == [value]
+
+def test_list_init_complex_values_raises_type_error(indigo: IndigoMock):
+    with pytest.raises(TypeError):
+        indigo.List(COMPLEX_VALUES)
+
+@pytest.mark.parametrize("value", COMPLEX_VALUES)
+def test_list_append_complex_values_raises_type_error(indigo: IndigoMock, value):
+    l = indigo.List()
+    with pytest.raises(TypeError):
+        l.append(value)
+
+def test_list_set_by_slice_raises_not_implemented_error(indigo: IndigoMock):
+    l = indigo.List()
+    with pytest.raises(NotImplementedError):
+        l[5:] = [1, 2, 3, 4, 5, 6] # type: ignore
+
+def test_list_get_by_slice_raises_not_implemented_error(indigo: IndigoMock):
+    l = indigo.List()
+    with pytest.raises(NotImplementedError):
+        l[5:] # type: ignore
+
+def test_list_to_list_simple(indigo: IndigoMock):
+    l = indigo.List([1, 2, 3, 4, 5])
+
+    assert l.to_list() == [1, 2, 3, 4, 5]
+
+def test_list_to_list_converts_nested_lists_and_dicts(indigo: IndigoMock):
+    l = indigo.List(
+        [
+            1,
+            2,
+            indigo.List([3, 4]),
+            indigo.Dict({"5": 5, "6": 6, "7": indigo.List([8, 9])}),
+            indigo.List([
+                indigo.Dict({"10": 10, "11": 11}),
+                indigo.Dict({"12": 12, "13": 13})
+            ])
+        ]
+    )
+
+    assert l.to_list() == [
+        1,
+        2,
+        [3, 4],
+        {"5": 5, "6": 6, "7": [8, 9]},
+        [{"10": 10, "11": 11}, {"12": 12, "13": 13}]
+    ]
+
+def test_list_values_return_copy(indigo: IndigoMock):
+    outer = indigo.List()
+    inner = indigo.List([1, 2, 3])
+    outer.append(inner)
+
+    assert outer[0] == inner
+    assert outer[0] is not inner
+
 def test_dict_get_key(indigo: IndigoMock):
     d = indigo.Dict()
     d["key"] = 5
@@ -20,50 +111,39 @@ def test_dict_get_missing_key_returns_default(indigo: IndigoMock):
     assert d.get("key", 5) == 5
 
 @pytest.mark.parametrize("key", ["abc123", "one23", "a/2", "A-_m!@"])
-def test_dict_accepts_valid_keys(indigo: IndigoMock, key):
+def test_dict_keys(indigo: IndigoMock, key):
     d = indigo.Dict()
     d[key] = "PYTEST"
 
     assert d[key] == "PYTEST"
 
 @pytest.mark.parametrize("key", [1, None, list()])
-def test_dict_rejects_non_string_key(indigo: IndigoMock, key):
-    with pytest.raises(Exception):
+def test_dict_non_string_key_raises_type_error(indigo: IndigoMock, key):
+    with pytest.raises(TypeError):
         d = indigo.Dict()
         d[key] = "PYTEST"
 
 @pytest.mark.parametrize("key", [" ", "ab c"])
-def test_dict_rejects_key_with_space(indigo: IndigoMock, key):
+def test_dict_key_with_space_raises_runtime_error(indigo: IndigoMock, key):
     with pytest.raises(RuntimeError, match="LowLevelBadParameterError -- illegal XML tag name character"):
         d = indigo.Dict()
         d[key] = "PYTEST"
 
 @pytest.mark.parametrize("key_start", list(string.digits + string.punctuation))
-def test_dict_rejects_key_starting_with_number_or_character(indigo: IndigoMock, key_start):
+def test_dict_key_starting_with_number_or_character_raises_runtime_error(indigo: IndigoMock, key_start):
     with pytest.raises(RuntimeError, match="LowLevelBadParameterError -- illegal XML tag name character"):
         d = indigo.Dict()
         d[key_start + "key"] = "PYTEST"
 
 @pytest.mark.xfail(reason="IndigoServer doesn't enforce this")
 @pytest.mark.parametrize("key", ["XML", "xml", "Xml"])
-def test_dict_rejects_xml_key(indigo: IndigoMock, key):
+def test_dict_xml_key_raises_runtime_error(indigo: IndigoMock, key):
     with pytest.raises(RuntimeError, match="LowLevelBadParameterError -- illegal XML tag name character"):
         d = indigo.Dict()
         d[key] = "PYTEST"
 
-@pytest.mark.parametrize(
-    "value",
-    [
-        True,
-        False,
-        1.8,
-        -7.5,
-        10,
-        "",
-        "pytest"
-    ]
-)
-def test_dict_accepts_primitive_values(indigo: IndigoMock, value):
+@pytest.mark.parametrize("value", PRIMITIVE_VALUES)
+def test_dict_set_primitive_value(indigo: IndigoMock, value):
     d = indigo.Dict()
     d["test"] = value
 
@@ -77,7 +157,7 @@ def test_dict_accepts_primitive_values(indigo: IndigoMock, value):
         {"one": 1, "two": [1, 2, 3]}
     ]
 )
-def test_dict_accepts_container_values(indigo: IndigoMock, value):
+def test_dict_set_container_values(indigo: IndigoMock, value):
     d = indigo.Dict()
     d["test"] = value
 
@@ -92,7 +172,7 @@ def test_dict_accepts_container_values(indigo: IndigoMock, value):
         {"key": {"key": {"key": Exception()}}}
     ]
 )
-def test_dict_rejects_complex_container_values(indigo: IndigoMock, value):
+def test_dict_complex_container_values_raises_type_error(indigo: IndigoMock, value):
     d = indigo.Dict()
     with pytest.raises(TypeError):
         d["test"] = value
