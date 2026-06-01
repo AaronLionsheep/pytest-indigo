d7d9e0c1cfcd98b32ebf0d890ad0cc1103aa67ad
Aaron Rohrbaugh <aaron@lionsheeptechnology.com>
2026-05-31 20:38:36 -0400

    Implement id generation for indigo objects

--- src/pytest_indigo/mock.py
+++ src/pytest_indigo/mock.py
@@ -1,11 +1,17 @@
 from pytest_indigo.indigo import Dict, List
+from pytest_indigo.indigo.ids import IndigoIds
+
 from types import ModuleType
 
 
 class IndigoMock(ModuleType):
+    Dict = Dict
+    List = List
+
+    ids: IndigoIds
+
     def __init__(self):
         self.reset()
 
     def reset(self):
-        self.Dict = Dict
-        self.List = List
+        self.ids = IndigoIds()
